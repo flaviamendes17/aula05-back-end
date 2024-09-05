bc4e805ee1cbdb11d8863a11d2607aff71a46251
Flavia Mendes <flavinharamendes@gmail.com>
2024-09-05 09:52:26 -0300

    feat: create get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -1,39 +1,61 @@
-import { Router } from "express"
+import { Router } from "express";
 
-const docesRoutes = Router()
+const docesRoutes = Router();
 
 const guloseimas = [
-    {
-        id:1,
-        nome:"Trufa",
-        preco:8.50
-    },
-    {
-        id:2,
-        nome:"Sorvete",
-        preco:25.00
-    },
-    {
-        id:3,
-        nome:"Pacoca",
-        preco:1.00
-    }
-]
+  {
+    id: 1,
+    nome: "Trufa",
+    preco: 8.5,
+  },
+  {
+    id: 2,
+    nome: "Sorvete",
+    preco: 25.0,
+  },
+  {
+    id: 3,
+    nome: "Pacoca",
+    preco: 1.0,
+  },
+];
 
+//Rota para buscar todos os elementos do array guloseimas.
 docesRoutes.get("/", (req, res) => {
-    return res.status(200).send(guloseimas)
-})
+  return res.status(200).send(guloseimas);
+});
+
+//Rota para criar nova guloseima.
 docesRoutes.post("/", (req, res) => {
-    const {nome,preco} = req.body 
+  const { nome, preco } = req.body;
 
-    const novoDoce = {
-        id: guloseimas.length + 1,
-        nome: nome,
-        preco: preco
-    }
+  const novoDoce = {
+    id: guloseimas.length + 1,
+    nome: nome,
+    preco: preco,
+  };
 
-    guloseimas.push (novoDoce)
-    return res.status(201).send(guloseimas)
+  guloseimas.push(novoDoce);
+  return res.status(201).send(guloseimas);
+});
+
+// Rota para buscar um elemento específico do array guloseimas.
+docesRoutes.get("/:id", (req, res) => {
+  const { id } = req.params
+
+  //console.log(id);
+  
+
+  const guloseima = guloseimas.find((doce) => doce.id === Number (id)
+)
+
+  console.log(guloseima)
+  
+
+  if(!guloseima){
+    return res.status(404).send ({ message: "Guloseima não encontrada!"})
+  }
+
+  return res.status(200).send (guloseima)
 })
-
 export default docesRoutes
